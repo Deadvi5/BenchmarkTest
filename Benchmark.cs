3ec47bdf928a1b087beca863e0e57d753a7e8e50
Lorenzo <lorenzo.villa.larry@gmail.com>
2020-09-06 14:59:09 +0200

    Create Benchmark.cs

---
+++ Benchmark.cs
@@ -0,0 +1,41 @@
+using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkTest.Mock;
+
+namespace BenchmarkTest
+{
+    public class Benchmark
+    {
+
+        private readonly IInfoParser serializer = new SerializerInfoParser();
+        private readonly IInfoParser stringBuilder = new StringInfoParser();
+
+        public Benchmark()
+        {
+        }
+
+        [Benchmark]
+        public void Serialize()
+        {
+            serializer.SaveInfo("testKey","testValue");
+            var result = serializer.GetInfo();
+
+            if(result.key == "testKey" && result.value == "testValue")
+            return;
+
+            throw new ApplicationException();
+        }
+
+        [Benchmark]
+        public void BuildString()
+        {
+            stringBuilder.SaveInfo("testKey","testValue");
+            var result = stringBuilder.GetInfo();
+
+            if(result.key == "testKey" && result.value == "testValue")
+            return;
+
+            throw new ApplicationException();
+        }
+    }
+}
